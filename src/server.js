dc779b09b1143fb14d9c8ba0bd5b90474bbab13a
Gage Eide <gageeide@gmail.com>
2020-06-28 10:27:53 -0500

    Pre-set up variables in config.js and imported where expected. 10:27am

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 const app = require('./app');
-const { PORT } = require('./config');
+const knex = require('knex');
+const { PORT, DATABASE_URL } = require('./config');
 
+// Create knex instance
+const db = knex({
+  client: 'pg',
+  connection: DATABASE_URL
+});
+
+// Set knex instance in app for easy access
+app.set('db', db);
+
+// Start server on given PORT
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`Server is listening at http://localhost:${PORT}`);
 });
